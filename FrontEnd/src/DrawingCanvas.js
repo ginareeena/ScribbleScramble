9689c6ed9e52942d7a65869e87a9d9e26e53de7f
ginareeena <ginamoffit@gmail.com>
2021-03-22 15:13:26 -0400

    feat(component/drawing): adds brush color and size change

--- FrontEnd/src/DrawingCanvas.js
+++ FrontEnd/src/DrawingCanvas.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { fabric } from "fabric";
 // import { copiedText } from "fabric/fabric-impl";
-import {Button} from './AppCSS'
+import { Button } from "./AppCSS";
 
 const DrawingCanvas = () => {
   const [canvas, setCanvas] = useState("");
@@ -19,75 +19,78 @@ const DrawingCanvas = () => {
       isDrawingMode: true,
     });
 
-  let selectElement = function (id) {
-    return document.getElementById("id");
-  };
+  // let getElement = function (id) {
+  //   return document.getElementById("id");
+  // };
 
-  // let brushColor;
-  // let brushWidth;
-  // let clear;
+  // var cursor = new fabric.StaticCanvas("cursor");
 
-  //   var brush = canvas.freeDrawingBrush=
+  // canvas.freeDrawingBrush.width = 20;
+  // canvas.freeDrawingBrush.color = "#ff0000";
 
-  // Can save current brush like this:
-  // canvas.freeDrawingBrush = brush
-  // canvas.freeDrawingBrush = diamondBrush? for ex
+  let drawingColorEl = document.getElementById("drawing-color");
+  let drawingModeEl = document.getElementById("drawing-mode-selector");
+  let drawingLineWidthEl = document.getElementById("drawing-line-width");
 
-  let brush = new fabric.PatternBrush(canvas);
+  let brushSizeText = document.getElementById("brushSize");
+  // let canvasEl = document.getElementById("canvas");
+  // let drawingModeEl = getElement("drawing-mode-selector");
+  // let drawingColorEl = getElement("drawing-color");
+  // // let drawingLineWidthEl = getElement("drawing-line-width");
 
-  //   let ctx = brush.getContext("2d");
-  //   ctx.fillStyle = "blue";
-  //   .width(20);
-  //   brush.color = "000FFF";
-  //   brush.getPatternSrc = function () {};
-  //   brush.strokeStyle = "blue";
-
-  // ADDS SQUARE:
-  //   var rect = new fabric.Rect({
-  //     width: 200,
-  //     height: 200,
-  //     // angle: 45,
-  //     fill: "blue",
-  //   });
-  //   canvas.add(rect);
-
-  //   function isDrawing() {
-  //     canvas.isDrawingMode();
-  //   let context = canvas.getContext("2d");
-  //   context.strokeStyle = "blue";
-  //   context.lineWidth = 5;
-
+  // function updateBrush(canvas){
+  //   if(canvas.freeDrawingBrush){
+  //     let brush=canvas.freeDrawingBrush
+  //     brush.width
   //   }
+  // }
 
-  //Brushes->
-  // Clear All
-  // Eraser
-  // Width
-  // Color
-  // Fun Brushes
+  // canvas.freeDrawingBrush = new fabric[drawingModeEl.value + "Brush"](canvas);
+
+  // canvasEl.onclick(updateBrush(canvas));
+
+  fabric.Object.prototype.transparentCorners = false;
+
+  // let brush = canvas.freeDrawingBrush;
+  function updateBrush(canvas) {
+    if (!canvas.freeDrawingBrush) {
+      canvas.freeDrawingBrush = new fabric[drawingModeEl.value + "Brush"]();
+    }
+    let brush = canvas.freeDrawingBrush;
+    brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+    brushSizeText.innerHTML = drawingLineWidthEl.value;
+    brush.color = drawingColorEl.value;
+    // canvas.renderAll();
+  }
 
   return (
     <div>
       <div id="drawing-mode-options">
         <label htmlFor="drawing-mode-selector">Mode:</label>
-        <select id="drawing-mode-selector">
-          <option>Pencil</option>
-          <option>Circle</option>
-          <option>Pattern</option>
+        <select id="drawing-mode-selector" onChange={() => updateBrush(canvas)}>
+          <option value="Pencil">Pencil</option>
+          <option value="Circle">Circle</option>
+          <option value="Pattern">Pattern</option>
         </select>
 
         <label htmlFor="drawing-line-width">Line width:</label>
-        <span className="info">30</span>
+        <span id="brushSize">11</span>
         <input
           type="range"
-          defaultValue="30"
+          defaultValue="11"
           min="0"
           max="150"
           id="drawing-line-width"
+          onChange={() => updateBrush(canvas)}
         />
 
         <label htmlFor="drawing-color">Line color:</label>
-        <input type="color" defaultValue="#005E7A" id="drawing-color" />
+        <input
+          type="color"
+          defaultValue="#005E7A"
+          id="drawing-color"
+          onChange={() => updateBrush(canvas)}
+        />
       </div>
 
       <h1>Drawing Canvas !</h1>
@@ -98,3 +101,75 @@ const DrawingCanvas = () => {
 };
 
 export default DrawingCanvas;
+
+// Other code i tried
+//could be useful for other brushes
+
+//Brushes to make->
+// Clear All X
+// Width  X
+// Color   X
+// Eraser
+// Fill Tool
+// Fun Brushes
+
+// function changeBrush(canvas, value) {
+//   canvas.freeDrawingBrush = new fabric[value + "Brush"](canvas);
+//   let brush = canvas.freeDrawingBrush;
+//   brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+// }
+
+// let pencil = new fabric.PencilBrush(canvas);
+// pencil.color = "blue";
+
+// function changeColor(canvas, value) {
+//   if (canvas.freeDrawingBrush) {
+//     let brush = canvas.freeDrawingBrush;
+//     // console.log("drawingColorEl", drawingColorEl);
+//     brush.color = value;
+//     brush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+//     console.log("value", value);
+//     // console.log("this", this);
+//   } else {
+//     // canvas.freeDrawingBrush = new fabric[value + "Brush"](canvas);
+//     // let brush = canvas.freeDrawingBrush;
+//     // brush.color = drawingColorEl.value;
+//     // console.log("drawingColorEl", drawingColorEl);
+//   }
+// }
+
+// let brushColor;
+// let brushWidth;
+// let clear;
+
+//   var brush = canvas.freeDrawingBrush=
+
+// Can save current brush like this:
+// canvas.freeDrawingBrush = brush
+// canvas.freeDrawingBrush = diamondBrush? for ex
+
+// let brush = new fabric.PatternBrush(canvas);
+
+//   let ctx = brush.getContext("2d");
+//   ctx.fillStyle = "blue";
+//   .width(20);
+//   brush.color = "000FFF";
+//   brush.getPatternSrc = function () {};
+//   brush.strokeStyle = "blue";
+
+// ADDS SQUARE:
+//   var rect = new fabric.Rect({
+//     width: 200,
+//     height: 200,
+//     // angle: 45,
+//     fill: "blue",
+//   });
+//   canvas.add(rect);
+
+//   function isDrawing() {
+//     canvas.isDrawingMode();
+//   let context = canvas.getContext("2d");
+//   context.strokeStyle = "blue";
+//   context.lineWidth = 5;
+
+//   }
