1db961243a4c05157462fb2f81fe999ba69e830e
ginareeena <ginamoffit@gmail.com>
2021-03-22 23:21:52 -0400

    feat(sockets): cleaned up files

--- FrontEnd/src/Socket.js
+++ FrontEnd/src/Socket.js
@@ -1,39 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// // import socketIOClient from "socket.io-client"
-// import socket from "socket.io-client";
-
-// export const clientSocket = socket(window.location.origin);
-
-// clientSocket.on("connect", () => {
-//   console.log("client socket!");
-// });
-
-// // const ENDPOINT = "http://127.0.0.1:4001";
-
-// const SocketComp = () => {
-//   const [response, setResponse] = useState("");
-
-//   useEffect(() => {
-//     const socket = socketIOClient(ENDPOINT);
-//     socket.on("FromAPI", (data) => {
-//       setResponse(data);
-//     });
-//     return () => socket.disconnect();
-//   }, []);
-
-//   return (
-//     <div>
-//       <p>
-//         It's <time dateTime={response}>{response}</time>
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default SocketComp;
-
-// socket.js;
-
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
