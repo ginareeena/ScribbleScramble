c10bdb6e02138cbee7b20a9cd51f2bc59c6d2fd4
Kelsey Greene <kelsey.greene17@gmail.com>
2021-03-23 21:07:09 -0400

    perf: deleted/updated redundant routes, components and unused code

--- FrontEnd/src/Routes.js
+++ FrontEnd/src/Routes.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WritingCanvas from "./WritingCanvas"
-import Drawing from "./Drawing";
-import String from "./String"
+import DrawingCanvas from "./DrawingCanvas";
+
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/write" component={WritingCanvas} />
-        <Route exact path="/draw" component={Drawing} />
-        <Route exact path="/string" component={String} />
+        <Route path="/write" component={WritingCanvas} />
+        <Route path="/draw" component={DrawingCanvas} />
       </Switch>
     </Router>
   );
