9b4d4484d9df31b35ff6e737eed393a4ca3ab91f
Meghan McFarland <meghanmc04@mac.com>
2021-03-25 17:59:37 -0400

    meta(socketts/frontend) rearranges code lines

--- FrontEnd/src/Socket.js
+++ FrontEnd/src/Socket.js
@@ -3,13 +3,14 @@ const SERVER = "http://127.0.0.1:4001";
 
 const socket = socketIOClient(SERVER);
 
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
-
 // maybe this should be connection?
 // index.js serverSocket is on "connection"
 socket.on("connect", () => {
   console.log("i'm connected with the front end!");
 });
+
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
+
 export default socket;
