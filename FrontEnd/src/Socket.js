4bcd3fb0c61edbb70fa43b5b22f51e3cc1a6c9dd
Meghan McFarland <meghanmc04@mac.com>
2021-03-25 17:08:17 -0400

    feat(sockets/users) adds a catch-all listener - any event received by the client will be printed in console

--- FrontEnd/src/Socket.js
+++ FrontEnd/src/Socket.js
@@ -1,11 +1,15 @@
 import socketIOClient from "socket.io-client";
 const SERVER = "http://127.0.0.1:4001";
 
-const socket = socketIOClient(SERVER, {autoConnect: false })
+const socket = socketIOClient(SERVER, { autoConnect: false });
+
+socket.onAny((event, ...args) => {
+  console.log(event, args);
+});
 
 //maybe this should be connection?
 // index.js serverSocket is on "connection"
-socket.on("connect", () => {
-  console.log("i'm connected with the front end!");
-});
+// socket.on("connect", () => {
+//   console.log("i'm connected with the front end!");
+// });
 export default socket;
