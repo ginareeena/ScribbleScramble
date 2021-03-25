7ef3685e9b1a5e459eef17a42f0e9c7ef419bc91
Meghan McFarland <meghanmc04@mac.com>
2021-03-25 17:39:43 -0400

    feat(component/username) creates handleSubmit + handleChange functionality for username input

--- FrontEnd/src/Username.js
+++ FrontEnd/src/Username.js
@@ -1,13 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { LandingPage } from "./AppCSS";
 
 const Username = () => {
+  const [username, setUsername] = useState("");
+  const [hasUsername, setHasUsername] = useState(false);
+
+  // useEffect( () => {
+  //   // setUsernameSelected(true)
+  // }, [username])
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    //need to control for just spaces!
+    if (username !== "") {
+      setHasUsername(true);
+      console.log("username is ", username, hasUsername);
+    } else {
+      alert("you didn't actually choose one tho...");
+    }
+  };
+
   return (
     <div>
       <LandingPage>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h6>please choose a username</h6>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            onChange={(evt) => setUsername(evt.target.value)}
+          />
           <br />
           <br />
           <input type="submit" value="Submit" />
