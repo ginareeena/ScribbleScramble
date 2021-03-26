89d43a134a6e679d440a4a79fa563bdd4b7f6d67
Meghan McFarland <meghanmc04@mac.com>
2021-03-26 13:59:16 -0400

    feat(components/waiting room) creates prelim waiting room component

---
+++ FrontEnd/WaitingRoom.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { LandingPage, Title2 } from "./AppCSS";
+
+const WaitingRoom = () => {
+  return (
+    <LandingPage>
+      <Title2>Waiting for other players...</Title2>
+    </LandingPage>
+  );
+};
