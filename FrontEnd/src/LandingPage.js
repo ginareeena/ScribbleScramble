b6c828dd86a86e4444197fdf8e267820b12b1587
Meghan McFarland <meghanmc04@mac.com>
2021-03-26 15:30:36 -0400

    feat(components/waiting room) adds 'chooseDraw' or 'chooseWrite' to landingBtns; sends users to waiting room instead of home page

--- FrontEnd/src/LandingPage.js
+++ FrontEnd/src/LandingPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import socket from "./Socket";
 import {
   StartDrawBtn,
   StartWriteBtn,
@@ -11,27 +11,31 @@ import {
 } from "./AppCSS";
 
 const LandingPageComp = () => {
+  const chooseDraw = () => {
+    console.log("emitting: player chose to draw");
+    socket.emit("player chose to draw", socket.username);
+  };
+
+  const chooseWrite = () => {
+    console.log("emitting: player chose to write");
+    socket.emit("player chose to write", socket.username);
+  };
+
   return (
     <div>
       <LandingPage>
         <LandingBtns>
-          <StartDrawBtn>
-            <a href="/draw" style={{ color: "black" }}>
-              <StartDrawImg>
-                {" "}
-                {/* <img src="/images/drawBtn.png" /> */}
-              </StartDrawImg>
+          <StartDrawBtn onClick={chooseDraw}>
+            <a href="/connecting" style={{ color: "black" }}>
+              <StartDrawImg />
               <LandingButton>Start Drawing Collab</LandingButton>
             </a>
           </StartDrawBtn>
         </LandingBtns>
         <LandingBtns>
-          <StartWriteBtn>
-            <a href="/write" style={{ color: "black" }}>
-              {" "}
-              <StartWriteImg>
-                {/* <img src="/images/writeBtn.png" /> */}
-              </StartWriteImg>
+          <StartWriteBtn onClick={chooseWrite}>
+            <a href="/connecting" style={{ color: "black" }}>
+              <StartWriteImg />
               <LandingButton>Start Writing Collab</LandingButton>
             </a>
           </StartWriteBtn>
