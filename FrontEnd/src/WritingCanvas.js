696b8fabf9731d8b52d59bb31e92d153c5a6fe28
Kelsey Greene <kelsey.greene17@gmail.com>
2021-03-26 14:43:06 -0400

    WIP: rendering text between clients

--- FrontEnd/src/WritingCanvas.js
+++ FrontEnd/src/WritingCanvas.js
@@ -7,15 +7,19 @@ import { CanvasBackground } from "./AppCSS";
 import socket from "./Socket";
 
 const WritingCanvas = () => {
-  const [textCanvas, setTextCanvas] = useState("");
-  const [font, setFont] = useState("arial");
-  const initCanvas = () =>
-    new fabric.Canvas("canvas", {
+  const initCanvas = () => {
+    const newCanvas = new fabric.Canvas("canvas", {
       height: 600,
       width: 800,
       isContentEditable: true,
-    });
-
+    
+    })
+    // newCanvas.setBackgroundColor("rgb(0, 191, 255)", newCanvas.renderAll.bind(newCanvas))
+    return newCanvas
+  }
+  const [textCanvas, setTextCanvas] = useState({});
+  const [font, setFont] = useState("arial");
+  
   /*
     Removed 'canvas' as a parameter in the initCanvas function, set add and renderAll
     methods to act on textCanvas.
@@ -25,28 +29,59 @@ const WritingCanvas = () => {
     back to an object for display?
 
     */
+    // useEffect(() => {
+    //   console.log('Inside setTextCanvas')
+    //   console.log('am i working ? textcanvas', textCanvas)
+    //   if (!Object.keys(textCanvas).length){
+    //   setTextCanvas(initCanvas());
+    //   }
+    // }, [textCanvas]);
+
   useEffect(() => {
-    setTextCanvas(initCanvas());
-    socket.on("create new text box", (value) => {
+    if (!Object.keys(textCanvas).length){
+        setTextCanvas(initCanvas());
+        }
+    else {
+    socket.on("create new text box", (value, textBox) => {
+      // console.log('useEffect value', value)
+      
+      
       // bringing back dataUrl and converting to image with src of the data url
       let textImage = new Image();
-      textImage.onload = () => {
-        return fabric.Image(textImage, 0, 0);
-      };
+      // textImage.onload = () => {
+      //   return new fabric.Image(textImage, 0, 0);
+      // };
+
       textImage.src = value;
       console.log("front end heard create new text box");
-      console.log('text image string: ', textImage)
-    });
-    const newText = new fabric.IText("Type here...", {
-      left: 150,
-      top: 100,
-      isContentEditable: true,
+      // console.log('text image string: ', textImage)
+      let socketText = textBox.objects[0]
+      console.log('useEffect socketText', socketText)
+      console.log('textCanvas', textCanvas)
+      console.log('textCanvas.add', textCanvas.add)
+      
+      textCanvas.add.bind(textCanvas, socketText)
+      console.log('after binding', textCanvas)
+      setTextCanvas(textCanvas)
+      //dummy url to see if there is an issue with our dataURL that fabric isn't liking
+      // fabric.loadSVGFromURL(textCanvas, function (val) {
+       //why is this undefined
+        // if (textCanvas){
+        //   textCanvas.add(val)
+        // }
+    })
+    }}, [textCanvas]);
+    // const newText = new fabric.IText("Type here...", {
+    //   left: 150,
+    //   top: 100,
+    //   isContentEditable: true,
       // fontFamily: font,
-    });
+    
     // POSSIBLY UNNECESSARY: 
-    // textCanvas.add(newText);
-    // textCanvas.renderAll();
-  }, []);
+    // if (textCanvas) 
+    //   {textCanvas.add(newText);
+    //   textCanvas.renderAll();}
+  
 
   const handleTextBtn = () => {
     const newText = new fabric.IText("Type here...", {
@@ -55,13 +90,12 @@ const WritingCanvas = () => {
       isContentEditable: true,
       fontFamily: font,
     });
-
-    textCanvas.add(newText);
-    textCanvas.renderAll();
+console.log("I am in handleTextButton!")
+    setTextCanvas(textCanvas.add(newText).renderAll())
+    // textCanvas.renderAll();
     let dataUrl = textCanvas.toDataURL();
     // sending data url to back end via socket
-    socket.emit("add text box", dataUrl);
-  };
+    socket.emit("add text box", dataUrl, textCanvas)
 
   const changeFont = (evt) => {
     setFont(evt.target.value);
@@ -84,7 +118,7 @@ const WritingCanvas = () => {
         <div id="text-options" style={{ marginTop: "10px" }}>
           <label htmlFor="font-family">Font:</label>
           <select id="font-family" value={font} onChange={changeFont}>
-            <option value="arial">Arial</option>
+            <option value="Arial">Arial</option>
             <option value="comic sans ms">Comic Sans MS</option>
             <option value="impact">Impact</option>
             <option value="monaco">Monaco</option>
--- index.js
+++ index.js
@@ -38,10 +38,10 @@ const serverSocket = require("socket.io")(http,
 
 serverSocket.on("connection", (socket) => {
   console.log(`server new client connected on ${socket.id}`);
-  socket.on("add text box", (value) => {
+  socket.on("add text box", (value, textCanvas) => {
     console.log("server side heard add text box!");
-    console.log("server/add text box value", value);
-    socket.broadcast.emit("create new text box", value);
+    // console.log("server/add text box value", value);
+    socket.broadcast.emit("create new text box", value, textCanvas);
   });
 });
 
