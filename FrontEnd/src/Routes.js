49c53d635393972ad4caa664524625931c0e93d5
Meghan McFarland <meghanmc04@mac.com>
2021-03-26 13:32:36 -0400

    feat(components/home) renders HomePage component on '/' instead of LandingPageComp

--- FrontEnd/src/Routes.js
+++ FrontEnd/src/Routes.js
@@ -2,17 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import WritingCanvas from "./WritingCanvas";
 import DrawingCanvas from "./DrawingCanvas";
-import LandingPageComp from "./LandingPage";
-import Username from "./Username";
+import Home from "./HomePage";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPageComp} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/write" component={WritingCanvas} />
         <Route exact path="/draw" component={DrawingCanvas} />
-        <Route path="/username" component={Username} />
       </Switch>
     </Router>
   );
