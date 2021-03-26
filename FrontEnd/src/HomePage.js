c76dd31bb1d8b86bff98c03df92e5132c030c174
Meghan McFarland <meghanmc04@mac.com>
2021-03-26 13:31:52 -0400

    feat(components/home) converts username into 'home' page - renders username input if no username, or landing page if username

---
+++ FrontEnd/src/HomePage.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { LandingPage } from "./AppCSS";
+import LandingPageComp from "./LandingPage";
+import socket from "./Socket";
+
+const Home = () => {
+  const [username, setUsername] = useState("");
+  const [hasUsername, setHasUsername] = useState(false);
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (username) {
+      setHasUsername(true);
+      socket.emit("add new player", username);
+    } else {
+      alert("please choose a valid username");
+    }
+  };
+
+  return (
+    <div>
+      {hasUsername ? (
+        <LandingPageComp />
+      ) : (
+        <LandingPage>
+          <form onSubmit={handleSubmit}>
+            <h6>please choose a username</h6>
+            <input
+              type="text"
+              name="username"
+              onChange={(evt) => setUsername(evt.target.value.trim())}
+            />
+            <br />
+            <br />
+            <input type="submit" value="Submit" />
+          </form>
+        </LandingPage>
+      )}
+    </div>
+  );
+};
+
+export default Home;
