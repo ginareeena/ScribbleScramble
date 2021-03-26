5681760bc4b9be31f88a3347e8cb00499409ddcf
Meghan McFarland <meghanmc04@mac.com>
2021-03-26 16:01:14 -0400

    feat(components/landing page) adds join game/create game functionality to landing page btns

--- FrontEnd/src/LandingPage.js
+++ FrontEnd/src/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import socket from "./Socket";
 import {
   StartDrawBtn,
@@ -11,30 +11,46 @@ import {
 } from "./AppCSS";
 
 const LandingPageComp = () => {
-  const chooseDraw = () => {
-    console.log("emitting: player chose to draw");
-    socket.emit("player chose to draw", socket.username);
+  const [gameReady, setGameReady] = useState(false);
+
+  const createGame = () => {
+    socket.emit("create new game", socket.id);
+    setGameReady(true);
   };
 
-  const chooseWrite = () => {
-    console.log("emitting: player chose to write");
-    socket.emit("player chose to write", socket.username);
+  const joinGame = () => {
+    socket.emit("player two wants to join", socket.id);
+    setGameReady(false);
   };
+
+  const handleClick = () => {
+    gameReady ? joinGame() : createGame();
+  };
+
+  // const chooseDraw = () => {
+  //   console.log("emitting: player chose to draw");
+  //   socket.emit("player chose to draw", socket.username);
+  // };
+
+  // const chooseWrite = () => {
+  //   console.log("emitting: player chose to write");
+  //   socket.emit("player chose to write", socket.username);
+  // };
 
   return (
     <div>
       <LandingPage>
         <LandingBtns>
-          <StartDrawBtn onClick={chooseDraw}>
-            <a href="/connecting" style={{ color: "black" }}>
+          <StartDrawBtn onClick={handleClick}>
+            <a href="/draw" style={{ color: "black" }}>
               <StartDrawImg />
               <LandingButton>Start Drawing Collab</LandingButton>
             </a>
           </StartDrawBtn>
         </LandingBtns>
         <LandingBtns>
-          <StartWriteBtn onClick={chooseWrite}>
-            <a href="/connecting" style={{ color: "black" }}>
+          <StartWriteBtn onClick={handleClick}>
+            <a href="/write" style={{ color: "black" }}>
               <StartWriteImg />
               <LandingButton>Start Writing Collab</LandingButton>
             </a>
