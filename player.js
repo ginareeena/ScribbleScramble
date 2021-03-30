9c6e54b2365c0b17952cfcb0ccdb5b999ec8be66
Meghan McFarland <meghanmc04@mac.com>
2021-03-30 16:58:58 -0400

    feat(player class) adds avatar to player class

--- player.js
+++ player.js
@@ -1,7 +1,8 @@
 module.exports = class Player {
-  constructor(socketID, username) {
+  constructor(socketID) {
     this.socketID = socketID;
-    this.username = username;
+    this.username = "";
+    this.avatar = "";
     this.isDrawing = false;
     this.isWriting = false;
     this.currentTurn = false;
@@ -9,6 +10,7 @@ module.exports = class Player {
   setUsername(name) {
     this.username = name;
   }
+  setAvatar() {}
   setIsDrawing() {}
   setIsWriting() {}
   setCurrentTurn() {}
