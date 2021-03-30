86d3fdcf0097cb0f86c6582e64b4236b43d7909d
Meghan McFarland <meghanmc04@mac.com>
2021-03-30 17:30:51 -0400

    feat(components/landing page) adds username form to landing page

--- FrontEnd/src/LandingPage.js
+++ FrontEnd/src/LandingPage.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import { useState } from "react";
+// import {useHistory} from 'react-router-dom'
 import {
   StartDrawBtn,
   StartWriteBtn,
@@ -9,43 +9,62 @@ import {
   StartDrawImg,
   StartWriteImg,
 } from "./AppCSS";
+import socket from "./Socket";
 
 const LandingPageComp = () => {
+  const [username, setUsername] = useState("");
+  // const history = useHistory()
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (username) {
+      socket.emit("add new player", username);
+    } else {
+      //MM: may be better option here!
+      alert("please choose a valid username");
+    }
+  };
+
   return (
     <div>
-      <LandingPage>
-        <LandingBtns>
-          <StartDrawBtn>
-            <a href="/draw" style={{ color: "black" }}>
-              <StartDrawImg>
-                {" "}
-                {/* <img src="/images/drawBtn.png" /> */}
-              </StartDrawImg>
-              <LandingButton>Start Drawing Collab</LandingButton>
-            </a>
-          </StartDrawBtn>
-        </LandingBtns>
-        <LandingBtns>
-          <StartWriteBtn>
-            <a href="/write" style={{ color: "black" }}>
-              {" "}
-              <StartWriteImg>
-                {/* <img src="/images/writeBtn.png" /> */}
-              </StartWriteImg>
-              <LandingButton>Start Writing Collab</LandingButton>
-            </a>
-          </StartWriteBtn>
-        </LandingBtns>
-        <StartDrawBtn>
+      <form onSubmit={handleSubmit}>
+        <LandingPage>
+          <LandingBtns>
+            <h6>Please choose a username:</h6>
+            <input
+              type="text"
+              name="username"
+              onChange={(evt) => setUsername(evt.target.value.trim())}
+            />
+          </LandingBtns>
+
+          <LandingBtns>
+            <StartDrawBtn>
+              <a href="/draw" style={{ color: "black" }}>
+                <StartDrawImg />
+                <LandingButton type="submit">
+                  Start Drawing Collab
+                </LandingButton>
+              </a>
+            </StartDrawBtn>
+          </LandingBtns>
+
+          <LandingBtns>
+            <StartWriteBtn type="submit">
+              <a href="/write" style={{ color: "black" }}>
+                <StartWriteImg />
+                <LandingButton>Start Writing Collab</LandingButton>
+              </a>
+            </StartWriteBtn>
+          </LandingBtns>
+          {/* <StartDrawBtn>
           <a href="/combined" style={{ color: "black" }}>
-            <StartDrawImg>
-              {" "}
-              {/* <img src="/images/drawBtn.png" /> */}
-            </StartDrawImg>
+            <StartDrawImg />
             <LandingButton>Combined Canvas</LandingButton>
           </a>
-        </StartDrawBtn>
-      </LandingPage>
+        </StartDrawBtn> */}
+        </LandingPage>
+      </form>
     </div>
   );
 };
