fa8478320cdd78d1adfe21b7e95fc7c1b5432636
Meghan McFarland <meghanmc04@mac.com>
2021-03-31 11:15:49 -0400

    chore(server) merge with main -- missing updated imports

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const Player = require("./player");
 const path = require("path");
 const morgan = require("morgan");
 const express = require("express");
-const app = express();
-const http = require("http").createServer(app);
+const app = express()
+const http = require("http")
+const baseServer = http.createServer()
+const cors = require("cors")
+
+const { Server } = require("socket.io")
+const cluster = require("cluster")
+const redisAdapter = require("socket.io-redis")
+// const redis = require("redis")
+const numCPUs = require("os").cpus().length;
+const { setupMaster, setupWorker } = require("@socket.io/sticky")
+// const sticky = require("socketio-sticky-session")
+
 const port = process.env.PORT || 4001;
 
 //middleware
@@ -12,7 +23,34 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "FrontEnd/build")));
+app.use(cors())
 
+<<<<<<< HEAD
+=======
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`)
+
+  const httpServer = http.createServer()
+  setupMaster(httpServer, {
+    loadBalancingMethod: "least-connection",
+  });
+  httpServer.listen(port)
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork()
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} started`)
+
+    const httpServer = http.createServer()
+    const io = new Server(httpServer)
+    io.adapter(redisAdapter({ host: "localhost", port: 6379}))
+    setupWorker(io);
+  })
+}
+
+>>>>>>> main
 //api routes
 app.get("/", (req, res, next) => {
   try {
@@ -32,11 +70,11 @@ app.use((err, req, res, next) => {
 });
 
 //sockets
-// might need to add heroku's path/port?
 const serverSocket = require("socket.io")(http, {
   cors: {
-    origins: ["http://localhost:3000", "http://localhost:4001"],
+    origin: ["http://localhost:3000", "http://localhost:4001"],
     methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
@@ -64,7 +102,6 @@ serverSocket.on("connection", (socket) => {
   //canvas
   socket.on("add text box", (value, textCanvas) => {
     console.log("server side heard add text box!");
-    // console.log("server/add text box value", value);
     socket.broadcast.emit("create new text box", value, textCanvas);
   });
   socket.on("send new lines", (value) => {
@@ -74,15 +111,7 @@ serverSocket.on("connection", (socket) => {
   });
 });
 
-// serverSocket.on("connection", (socket) => {
-//   console.log(`server new client connected on ${socket.id}`);
-//   socket.on("drawing", (value) => {
-//     console.log("server side heard drawing!");
-//     console.log("drawing value received in back: --->", value);
-//     socket.broadcast.emit("adding to drwing", value);
-//   });
-// });
 
-http.listen(port, () => {
+baseServer.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
