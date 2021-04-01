0e3b3ff29cb957e5a3d97094f29b532b85ba4b89
Meghan McFarland <meghanmc04@mac.com>
2021-04-01 13:48:38 -0400

    feat(carousel/icons) creates file to export icon images

---
+++ FrontEnd/src/Icons.js
@@ -0,0 +1,8 @@
+const icons = {
+  bird: <img src="../public/avatars/images/bird.jpg" />,
+  cake: <img src="../public/avatars/images/cake.jpg" />,
+  catzoom: <img src="../public/avatars/images/catzoom.jpg" />,
+  cotton: <img src="../public/avatars/images/cotton.jpg" />,
+};
+
+export default icons;
