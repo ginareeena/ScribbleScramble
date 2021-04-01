54ba1593e9cf63cf7bbfe49e3e15a247dd66c25b
Meghan McFarland <meghanmc04@mac.com>
2021-04-01 14:48:11 -0400

    feat(carousel/routes) removes test route for carousel

--- FrontEnd/src/Routes.js
+++ FrontEnd/src/Routes.js
@@ -7,7 +7,6 @@ import EndGame from "./EndGame";
 import CombinedCanvas from "./CombinedCanvas";
 // Importing Modal for testing
 import ModalComp from "./Modal";
-import AvatarCarousel from "./AvatarCarousel";
 
 const Routes = () => {
   return (
@@ -20,7 +19,6 @@ const Routes = () => {
         <Route path="/combined" component={CombinedCanvas} />
         {/* Route for testing Modal component */}
         <Route path="/testing" component={ModalComp} />
-        <Route path="/carousel-test" component={AvatarCarousel} />
       </Switch>
     </Router>
   );
