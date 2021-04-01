16e84a324dc4a2658b095e7307c247fd20870090
kristen <studiosok@gmail.com>
2021-04-01 13:00:58 -0400

    feat(roomz) Add basic room logic in back end, ready to connect on front end

--- index.js
+++ index.js
@@ -46,7 +46,28 @@ const serverSocket = require("socket.io")(http, {
   // transports: ["websocket"]
 });
 
+
+
+
 serverSocket.on("connection", (socket) => {
+  let PRIVATE = "private"
+  let PUBLIC = "public"
+
+  // put emits in onClicks in front end buttons; if errors, use front end emit to give the room id
+  socket.on('joinPublicRoom', () => {
+    io.of("/").adapter.on("join-room", (PUBLIC, id) => {
+      socket.join(PUBLIC)
+      console.log(`socket ${id} has joined room ${room}`);
+    });
+  })
+
+  socket.on('joinPrivateRoom', () => {
+    io.of("/").adapter.on("join-room", (PRIVATE, id) => {
+      socket.join(PRIVATE)
+      console.log(`socket ${id} has joined room ${room}`);
+    });
+  })
+
   console.log(`server new client connected on ${socket.id}`);
   socket.on("add text box", (value, textCanvas) => {
     console.log("server side heard add text box!");
@@ -59,7 +80,38 @@ serverSocket.on("connection", (socket) => {
   });
 });
 
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage"
+
+//this variable will increment inside socket
+
+//namespace -> 'nsp' plan had been scribb..../room##; make own room name/hardcoded ?
+//using socket.io's built in adapter instead of the redis-based adapter
+
+// When creating new room, make sure new room is rendering new instance of the canvas
+
+//base case: 2 rooms. 1 private, 1 public
+//after that, expand private room ability to be more exclusive
+
+    //work in progress - room functionality
+    // let roomNo = 1;
+    // //  const {roomId} = socket.handshake.query
+    //  if(serverSocket.nsps['/'].adapter.rooms[roomNo] && serverSocket.nsps['/'].adapter.rooms[roomNo].length > 1)
+    //     {roomNo++
+    //     socket.join(roomNo)
+    //     }
+    
+    //  //look for new messages
+    //  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    //    serverSocket.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data)
+    //  })
+ 
+    //   socket.on("send new lines", (value) => {
+    //     console.log("server side heard drawing from front end!");
+    //     console.log("drawing value received in back: --->", value);
+    //     socket.broadcast.emit("load new lines", value);
+    //   });
+    // })
+
 http.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
-
