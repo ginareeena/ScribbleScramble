0052a9ca154466d63e09f0e303b35899056b5511
ginareeena <ginamoffit@gmail.com>
2021-04-02 18:11:55 -0400

    feat(CSS): updates flower image and adds CSS to End Game Page

--- FrontEnd/src/EndGame.js
+++ FrontEnd/src/EndGame.js
@@ -39,14 +39,11 @@ const EndGame = (props) => {
     <EndGamePage>
       <EndGameContainer>
         <EndGameH1>Thanks For Playing!</EndGameH1>
-        <EndGameElement>Save your Scribb Scrabb?</EndGameElement>
+        <EndGameElement>What a Beautiful Scribb Scrabb!</EndGameElement>
         <EndGameElement>
-          <img src={finalScribs} style={{ width: "300px", margin: "10px" }} />
+          <img src={finalScribs} style={{ width: "450px", margin: "10px" }} />
         </EndGameElement>
-        <DownloadAhref
-          href={finalScribs}
-          download="ScribScrab.png"
-        >
+        <DownloadAhref href={finalScribs} download="ScribScrab.png">
           Download
         </DownloadAhref>
 
