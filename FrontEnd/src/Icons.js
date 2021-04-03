7a4f02839d20790161436589f26930da51d8cb72
Meghan McFarland <meghanmc04@mac.com>
2021-04-03 19:28:46 -0400

    feat(avatars) adds new avatars and organizes avatars file

--- FrontEnd/src/Icons.js
+++ FrontEnd/src/Icons.js
@@ -1,54 +1,71 @@
+export const beet = process.env.PUBLIC_URL + "/images/avatars/beet.png";
 export const bird = process.env.PUBLIC_URL + "/images/avatars/bird.png";
-
 export const cake = process.env.PUBLIC_URL + "/images/avatars/cake.png";
-
+export const cat = process.env.PUBLIC_URL + "/images/avatars/cat.png";
 export const catzoom = process.env.PUBLIC_URL + "/images/avatars/catzoom.png";
-
+export const coffee = process.env.PUBLIC_URL + "/images/avatars/coffee.png";
 export const cotton = process.env.PUBLIC_URL + "/images/avatars/cotton.png";
-
 export const crab = process.env.PUBLIC_URL + "/images/avatars/crab.png";
-
+export const creature = process.env.PUBLIC_URL + "/images/avatars/creature.png";
 export const deer = process.env.PUBLIC_URL + "/images/avatars/deer.png";
-
+export const egg = process.env.PUBLIC_URL + "/images/avatars/egg.png";
 export const firefly = process.env.PUBLIC_URL + "/images/avatars/firefly.png";
-
 export const fish = process.env.PUBLIC_URL + "/images/avatars/fish.png";
-
+export const flower = process.env.PUBLIC_URL + "/images/avatars/flower.png";
+export const horse = process.env.PUBLIC_URL + "/images/avatars/horse.png";
+export const fly = process.env.PUBLIC_URL + "/images/avatars/fly.png";
 export const icecream = process.env.PUBLIC_URL + "/images/avatars/icecream.png";
-
+export const link = process.env.PUBLIC_URL + "/images/avatars/link.png";
+export const pika = process.env.PUBLIC_URL + "/images/avatars/pika.png";
 export const pizza = process.env.PUBLIC_URL + "/images/avatars/pizza.png";
-
+export const pretzel = process.env.PUBLIC_URL + "/images/avatars/pretzel.png";
 export const sandwich = process.env.PUBLIC_URL + "/images/avatars/sandwich.png";
-
+export const ski = process.env.PUBLIC_URL + "/images/avatars/ski.png";
 export const spiderman =
   process.env.PUBLIC_URL + "/images/avatars/spiderman.png";
-
 export const storm = process.env.PUBLIC_URL + "/images/avatars/storm.png";
-
 export const taxi = process.env.PUBLIC_URL + "/images/avatars/taxi.png";
-
 export const tomato = process.env.PUBLIC_URL + "/images/avatars/tomato.png";
-
 export const violin = process.env.PUBLIC_URL + "/images/avatars/violin.png";
-
-export const whale = process.env.PUBLIC_URL + "/images/avatars/whale.png";
-
-// new ones
-
-export const beet = process.env.PUBLIC_URL + "/images/avatars/beet.png";
-export const cat = process.env.PUBLIC_URL + "/images/avatars/cat.png";
-export const coffee = process.env.PUBLIC_URL + "/images/avatars/coffee.png";
-export const creature = process.env.PUBLIC_URL + "/images/avatars/creature.png";
-export const horse = process.env.PUBLIC_URL + "/images/avatars/horse.png";
-export const link = process.env.PUBLIC_URL + "/images/avatars/link.png";
 export const wasp = process.env.PUBLIC_URL + "/images/avatars/wasp.png";
-export const ski = process.env.PUBLIC_URL + "/images/avatars/ski.png";
+export const watermelon =
+  process.env.PUBLIC_URL + "/images/avatars/watermelon.png";
+export const whale = process.env.PUBLIC_URL + "/images/avatars/whale.png";
+export const witch = process.env.PUBLIC_URL + "./images/avatars/witch.png";
 
-export const flower = process.env.PUBLIC_URL + "/images/avatars/flower.png";
+const icons = [
+  beet,
+  bird,
+  cake,
+  cat,
+  catzoom,
+  coffee,
+  cotton,
+  crab,
+  creature,
+  deer,
+  egg,
+  firefly,
+  fish,
+  flower,
+  fly,
+  horse,
+  icecream,
+  link,
+  pika,
+  pizza,
+  pretzel,
+  sandwich,
+  ski,
+  spiderman,
+  storm,
+  taxi,
+  tomato,
+  violin,
+  wasp,
+  watermelon,
+  whale,
+  witch,
+];
 
-//   cake: <img src="../public/avatars/images/cake.jpg" />,
-//   catzoom: <img src="../public/avatars/images/catzoom.jpg" />,
-//   cotton: <img src="../public/avatars/images/cotton.jpg" />,
-// };
-
-// export default icons;
+export default icons;
