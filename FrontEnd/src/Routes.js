db7286b5112347184a6ade9f83be5093a283ead1
Meghan McFarland <meghanmc04@mac.com>
2021-04-03 19:02:13 -0400

    chore(file cleanup) deletes unused routes from Routes component

--- FrontEnd/src/Routes.js
+++ FrontEnd/src/Routes.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import WritingCanvas from "./WritingCanvas";
-import DrawingCanvas from "./DrawingCanvas";
 import LandingPageComp from "./LandingPage";
 import EndGame from "./EndGame";
 import CombinedCanvas from "./CombinedCanvas";
@@ -11,10 +9,9 @@ const Routes = () => {
     <Router>
       <Switch>
         <Route exact path="/" component={LandingPageComp} />
-        <Route path="/write" component={WritingCanvas} />
-        <Route path="/draw" component={DrawingCanvas} />
-        <Route path="/endgame" component={EndGame}/>
         <Route path="/scramble/:room" component={CombinedCanvas}/>
+        <Route path="/endgame" component={EndGame}/>
+
       </Switch>
     </Router>
   );
