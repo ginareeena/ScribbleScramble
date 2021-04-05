5a9f5f45318716346a40b4e4dc70a2548341d4a8
Kelsey Greene <kelsey.greene17@gmail.com>
2021-04-05 15:37:18 -0400

    feat: chatbox - bullet points/numbering removed

--- FrontEnd/src/ChatBox.js
+++ FrontEnd/src/ChatBox.js
@@ -1,43 +1,42 @@
 import React, { useState } from "react";
-import UsingChat from "./UsingChat";
+import UsingChat from "./UsingChat"
 
 const ChatBox = (props) => {
-  console.log("chatbox props", props);
-  const { room } = props;
-  const { messages, sendMessage } = UsingChat(room);
-  const [newMessage, setNewMessage] = useState("");
-  console.log("in Chatbox - arr of Messages:", messages);
+  console.log('chatbox props', props)
+const {room} = props
+const {messages, sendMessage} = UsingChat(room);
+const [newMessage, setNewMessage] = useState("")
+console.log('in Chatbox - arr of Messages:', messages)
 
-  const handleTypingMessage = (event) => {
-    setNewMessage(event.target.value);
-  };
-  const handleSubmit = () => {
-    sendMessage(newMessage);
-    setNewMessage("");
-  };
 
-  return (
+const handleTypingMessage = (event) => {
+  setNewMessage(event.target.value)
+}
+const handleSubmit = () => {
+   sendMessage(newMessage)
+   setNewMessage('')
+    }
+
+
+return (
     <div id="chat box">
       <div>
-        <h4> Scribble a note to your partner!</h4>
+        <h4> Scribble a note to your partner!</h4>  
       </div>
       <div>
-        <ol className="messageList">
-          {messages.map((message, index) => (
-            <li
-              key={index}
-              className={`single-message ${
-                message.ownedByCurrentPlayer ? "my-message" : "received-message"
-              }`}
-            >
-              {message.body}
-            </li>
-          ))}
-        </ol>
+      <ul className="messageList" style={{listStyleType: "none"}}>
+        {messages.map((message, index) => (
+          <li key={index}
+          className={`single-message ${
+            message.ownedByCurrentPlayer ? "my-message" : "received-message"
+          }`} 
+          >
+            {message.body}
+          </li>
+        ))}
+      </ul>
       </div>
-      <textarea
-        value={newMessage}
-        onChange={handleTypingMessage}
+      <textarea value= {newMessage} onChange={handleTypingMessage}
         placeholder="Write something..."
         className="new-message-input"
       />
@@ -45,7 +44,7 @@ const ChatBox = (props) => {
         Send
       </button>
     </div>
-  );
-};
+)
+}
 
-export default ChatBox;
+export default ChatBox
