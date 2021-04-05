c895dff3b476459dc2fa3e5b0594ca46af40f3e4
Meghan McFarland <meghanmc04@mac.com>
2021-04-05 15:41:19 -0400

    chore(CLEAN) removes console.logs/comments from UsingChat.js

--- FrontEnd/src/UsingChat.js
+++ FrontEnd/src/UsingChat.js
@@ -6,16 +6,13 @@ const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 
 const UsingChat = (room) => {
   console.log('I am in Using Chat react hook!')
-  const [messages, setMessages] = useState([]);//all sent and received messages
+  const [messages, setMessages] = useState([]);
   const socketRef = useRef();
-  console.log('in usingChat react hook messages', messages)
-  console.log('socketRefxs', socketRef.current)
 
   useEffect(() => {
     socketRef.current = socketIoClient(SOCKET_SERVER_URL, {
       query: { room: room }
     });
-
     socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, (message) => {
       const incomingMessage = {
         ...message,
@@ -23,7 +20,6 @@ const UsingChat = (room) => {
       };
       setMessages((messages) => [...messages, incomingMessage]);
     });
-
     return () => {
       socketRef.current.disconnect();
     };
