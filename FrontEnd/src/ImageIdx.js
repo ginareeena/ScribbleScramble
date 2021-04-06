02be4fbca27563ba851d0f404efa8a2f5b4d8e2c
ginareeena <ginamoffit@gmail.com>
2021-04-05 23:55:46 -0400

    feat(gallery): adds images to gallery

--- FrontEnd/src/ImageIdx.js
+++ FrontEnd/src/ImageIdx.js
@@ -1,17 +1,38 @@
 export const imageSet = [
   {
-    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/coolCat.png`,
-    alt: "Illustrated cool cat with assorted words",
-    idx: 0
-  },
-  {
-    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/ScribScrab.png`,
-    alt: "Poem with illustration of large eyeballs",
-    idx: 1
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/batty_gallery.png`,
+    alt: "Bat Poem",
+    idx: 0,
   },
   {
     imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/ScribScrab1.png`,
     alt: "Pizza slice with fish called 'Pizza Heaven'",
-    idx: 2
+    idx: 1,
+  },
+  {
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/butterfly.png`,
+    alt: "Butterfly and Sun Saying Whee and Shine",
+    idx: 2,
+  },
+  {
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/coolCat.png`,
+    alt: "Illustrated cool cat with assorted words",
+    idx: 3,
+  },
+  {
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/unicorn.png`,
+    alt: "A sun and unicorn vibing",
+    idx: 4,
+  },
+  {
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/ScribScrab.png`,
+    alt: "Poem with illustration of large eyeballs",
+    idx: 5,
+  },
+
+  {
+    imageURL: `${process.env.PUBLIC_URL}/images/GalleryImages/catplants.png`,
+    alt: "cats and plants",
+    idx: 6,
   },
 ];
