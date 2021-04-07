c13bb7385ccb08aea73cf3e35565ce03db61413a
ginareeena <ginamoffit@gmail.com>
2021-04-07 12:07:13 -0400

    feat(logo): applies higher res logo

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -10,7 +10,11 @@ function App() {
         <Header className="App-header">
           <Title>
             <a href="/">
-              <img src="/images/logoSm.png" alt="scribble scramble logo" />
+              <img
+                style={{ width: "800px" }}
+                src="/images/logoBig.png"
+                alt="scribble scramble logo"
+              />
             </a>
           </Title>
         </Header>
